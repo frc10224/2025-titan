457fa9be41cfbc0d0828ad7933bbc23ab75eaaae
Matthew Wozniak <me@woz.blue>
2025-02-03 16:44:40 -0500

    feat: add drive PID values

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -2,27 +2,25 @@
 
 #include <frc/geometry/Translation2d.h>
 #include <frc/geometry/Transform3d.h>
+#include <units/length.h>
 #include <units/base.h>
 
 namespace DrivetrainConstants {
-    // TODO: fix these!
     const double kMaxDriveSpeed = 1;
     const double kMaxTurnSpeed = 1;
-    const double kGearRatio = 5.71;
-    const units::inch_t kWheelRadius = 2_in;
-    const double kP = 0.001;
+    const double kGearRatio = 1/5.71;
+    const units::meter_t kWheelRadius = 2_in;
+    const double kP = 0.0000026155;
     const double kI = 0;
-    const double kD = 0.001;
-    // https://docs.revrobotics.com/brushless/neo/v1.1#motor-specifications
-    // This value should be 1/Kv
-    const double kVelocityFF = 1/473;
+    const double kD = 0;
+    const double kVelocityFF = 0.11911;
     // Max power RPM
     const double kMaxRPM = 2900;
     // CAN bus IDs
-    const int kMotorId_LB = 3;
-    const int kMotorId_LF = 4;
-    const int kMotorId_RF = 1;
-    const int kMotorId_RB = 2;
+    const int kMotorId_LB = 2;
+    const int kMotorId_LF = 1;
+    const int kMotorId_RF = 3;
+    const int kMotorId_RB = 4;
     const frc::Translation2d kFrontLeftLocation{0.381_m, 0.381_m};
     const frc::Translation2d kFrontRightLocation{0.381_m, -0.381_m};
     const frc::Translation2d kBackLeftLocation{-0.381_m, 0.381_m};
