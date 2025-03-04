4716fc2432994f1461fddd84424e97bf695f586f
Matthew Wozniak <me@woz.blue>
2025-03-04 18:45:15 -0500

    feat: preliminary pv code

---
+++ src/main/java/frc/robot/Pose.java
@@ -0,0 +1,45 @@
+package frc.robot;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import static frc.robot.Constants.PoseConstants.*;
+
+// this may not be a "subsystem" but it does contain a lot of things that
+// should be self contained
+
+public final class Pose {
+    private static final Pose instance = new Pose();
+    public static Pose getInstance() { return instance; }
+
+    PhotonCamera camera = new PhotonCamera("camera");
+    
+    Pose3d poseEstimate;
+    AHRS navx = new AHRS(NavXComType.kMXP_SPI);
+    AprilTagFieldLayout tagLayout =
+        AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
+
+    public void periodicUpdate() {
+        for (PhotonPipelineResult result : camera.getAllUnreadResults()) {
+            // Calculate robot's field relative pose
+            PhotonTrackedTarget target = result.getBestTarget();
+            if (tagLayout.getTagPose(target.getFiducialId()).isPresent()) {
+                Pose3d robotPose =  PhotonUtils.estimateFieldToRobotAprilTag(
+                    target.getBestCameraToTarget(),
+                    tagLayout.getTagPose(target.getFiducialId()).get(),
+                    kCameraLocation);
+                //SmartDashboard.putNumberArray("Pose/Estimate", {robotPose.getMeasureX().as(Meters), robotPose.getMeasureY(), robotPose.getMeasureZ()});
+            }
+        }
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -17,12 +18,19 @@ public class Robot extends TimedRobot {
 	public Robot() {}
 
 	@Override
+	public void robotInit() {
+		CameraServer.startAutomaticCapture();
+	}
+
+	@Override
 	public void robotPeriodic() {
 		// Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
 		// commands, running already-scheduled commands, removing finished or interrupted commands,
 		// and running subsystem periodic() methods.  This must be called from the robot's periodic
 		// block in order for anything in the Command-based framework to work.
 		CommandScheduler.getInstance().run();
+		// Update pose stuff
+		Pose.getInstance().periodicUpdate();
 	}
 
 	@Override
